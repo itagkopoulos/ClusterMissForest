a7bccdf567e145c22d85827e944ff26334aa3d59
fangzhouli <fzli0805@gmail.com>
2019-09-10 16:35:40 -0700

    added comments

--- util.py
+++ util.py
@@ -2,19 +2,6 @@ from collections import defaultdict
 import csv
 import numpy as np 
 
-def v_parse(val):
-    """parse a string to float (continuous) or string (discrete)"""
-    try:
-        # check if value is int (discrete)
-        val = str(int(val))
-    except:
-    	try:
-    		val = float(val)
-    	except:
-    		val = val 
-
-    return val 
-
 def read_csv(file, header=False):
     """return a parsed csv file in built-in dataframe"""
     data = []
@@ -29,13 +16,14 @@ def read_csv(file, header=False):
     return data 
 
 def isnan(x):
-    print(x)
-    if isinstance(x, np.floating):
+    """return boolean for checking if value is nan"""
+    if isinstance(x, (float, np.floating)):
         return np.isnan(x)
     else:
         return x == 'nan'
 
 def mode(x):
+    """return the mode of a list"""
 	count = defaultdict(int)
 	for item in x:
 		count[item] += 1
